fb4d055ac3ba24315e1fe9045590fdf66a5250a8
Lea Bugeat <lbugeat@gmail.com>
2021-06-01 22:40:27 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroy all"
+Task.destroy_all
+
+task_1 = Task.create(title: "Faire une lessive", details: "Blanc / heures creuses midi")
+task_2 = Task.create(title: "Faire la liste des courses", details: "Attention aux promos")
+task_3 = Task.create(title: "Aller à la décharge", details: "")
+task_4 = Task.create(title: "Prendre rendez-vous dentiste", details: "Trouver une bon dentiste")
+task_5 = Task.create(title: "Aller à la poste", details: "Ne pas oublier avis de passage")
+
+
+
+
